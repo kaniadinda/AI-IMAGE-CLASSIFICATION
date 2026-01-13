e0f79e82fb707c0870e42b3e56e0062e72d91c93
unknown <kaniadinda21@gmail.com>
2026-01-13 14:47:01 +0700

    Add dataset download link

--- deploy.py
+++ deploy.py
@@ -2,12 +2,20 @@ import streamlit as st
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+import os
+import gdown
 from PIL import Image, ImageOps
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 img_size = 48
 
-model = tf.keras.models.load_model("AIGeneratedModel.h5")
+MODEL_PATH = "AIGeneratedModel.h5"
+
+if not os.path.exists(MODEL_PATH):
+    url = "https://drive.google.com/uc?id=1uQQZvi9p5ZTKCr7ZDImBV7z3ox1D_AZd"
+    gdown.download(url, MODEL_PATH, quiet=False)
+
+model = tf.keras.models.load_model(MODEL_PATH)
 
 st.title("AI Image Classifier")       
         
